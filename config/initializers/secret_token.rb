d0be97ec83839811245e964954a5eb3c7e23a5ae
Corey Ford <coyotebush22@gmail.com>
2013-02-13 10:12:28 -0800

    initial Rails 3.2.12 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Permalinq::Application.config.secret_token = 'de979f4e0ed2210a794fc20fe882c2463edde7c8f4b30a67634ccf8ff3c90751502870381cdc6db6b009e4f6b4b55cb6cff3047ba147196d9f26a13637dea2e0'
