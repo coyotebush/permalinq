843cb13593dd1e51dcda1b8662fd3f585ba6b78d
Corey Ford <coyotebush22@gmail.com>
2013-02-21 19:58:17 -0800

    default to pages list

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Permalinq::Application.routes.draw do
   resources :pages
 
+  root :to => 'pages#index'
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
