6ad8a51e90c239056eb2a65084a8be97bb4caaa8
Corey Ford <coyotebush22@gmail.com>
2013-03-02 15:00:22 -0800

    require name and date for events

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,6 @@ class Event < ActiveRecord::Base
   has_many :projects, :through => :relevances
 
   attr_accessible :date, :description, :location, :name
+  validates :date, :presence => true
+  validates :name, :presence => true
 end
--- test/unit/event_test.rb
+++ test/unit/event_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not be valid without a date" do
+    event = Event.new
+    event.name = "foo"
+    assert !event.valid?, "Event is valid without a date"
+  end
+
+  test "should not be valid without a name" do
+    event = Event.new
+    event.date = 2.days.ago
+    assert !event.valid?, "Event is valid without a name"
+  end
+
+  test "should require a date and name" do
+    event = Event.new
+    event.date = 2.days.ago
+    event.name = "foo"
+    assert event.valid?, "Event is not valid with a date and name"
+  end
 end
