3cb2a49cfeb538709c399f06de9bb6dfce24fc15
Corey Ford <coyotebush22@gmail.com>
2013-03-07 18:20:38 -0800

    styling for future events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,13 @@
 class Event < ActiveRecord::Base
   has_many :relevances
   has_many :projects, :through => :relevances
+  default_scope order("date DESC")
 
   attr_accessible :date, :description, :location, :name, :project_ids
   validates :date, :presence => true
   validates :name, :presence => true
+
+  def future?
+    return date > DateTime.now
+  end
 end
